e3f741ae8a7aaa301f1dab91fd71b8e0eadeceb6
Ali Yoosefi <yoosefi12@gmail.com>
2019-01-19 14:59:21 +0330

    - first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,6 @@
+import ShebaUtil.validateIranianSheba
+
+object Main extends App {
+  println(validateIranianSheba("IR760120010000001659069109"))
+  println(validateIranianSheba("IR760120010000001659069108"))
+}
---
+++ src/main/scala/ShabaUtil.scala
@@ -0,0 +1,25 @@
+object ShebaUtil {
+  private def mod(num: String, a: Int) = {
+    def c(num: String, a: Int, i: Int): Long = if (i < 0) 0 else (c(num, a, i - 1) * 10 + num.charAt(i).toInt - '0') % a
+
+    c(num, a, num.length - 1)
+  }
+
+  private def iso7064Mod97_10(iban: String) = mod(iban, 97)
+
+  def validateIranianSheba(str: String): Boolean = {
+    val pattern = "IR[0-9]{24}"
+    str match {
+      case x if x.length != 26 => false
+      case x if !x.matches(pattern) => false
+      case x =>
+        val n = x.substring(0, 2)
+        val m = x.substring(2, 4)
+        val o = x.substring(4)
+        val d1 = n.charAt(0).toInt - 65 + 10
+        val d2 = n.charAt(1).toInt - 65 + 10
+        val newStr = s"$o$d1$d2$m"
+        iso7064Mod97_10(newStr) == 1
+    }
+  }
+}
